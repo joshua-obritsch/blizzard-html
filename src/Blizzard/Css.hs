2285fa53cc3a1bcd38d7eee40c45b309ed62b350
Joshua Obritsch <joshua@obritsch.com>
2022-06-01 22:10:28 +0200

    Install Clay

--- src/Blizzard/Css.hs
+++ src/Blizzard/Css.hs
@@ -1,6 +1,46 @@
 module Blizzard.Css
-    ( module Blizzard.Css.Border
+    ( module Clay.Background
+    , module Clay.Border
+    , module Clay.Box
+    , module Clay.Color
+    , module Clay.Common
+    , module Clay.Display
+    , module Clay.Dynamic
+    , module Clay.Flexbox
+    , module Clay.Font
+    , module Clay.FontFace
+    , module Clay.Geometry
+    , module Clay.Gradient
+    , module Clay.List
+    , module Clay.Size
+    , module Clay.Text
+    , module Clay.Time
+    , module Clay.Transform
+    , module Clay.Transition
+    , module Clay.Animation
+    , module Clay.Mask
+    , module Clay.Filter
     ) where
 
 
-import Blizzard.Css.Border
+import Clay.Background
+import Clay.Border
+import Clay.Box
+import Clay.Color
+import Clay.Common
+import Clay.Display hiding (table)
+import Clay.Dynamic
+import Clay.Flexbox hiding (flex, nowrap, wrap)
+import Clay.Font hiding (menu, caption, small, icon)
+import Clay.FontFace
+import Clay.Geometry
+import Clay.Gradient
+import Clay.List
+import Clay.Size
+import Clay.Text hiding (pre)
+import Clay.Time
+import Clay.Transform
+import Clay.Transition
+import Clay.Animation
+import Clay.Mask hiding (clear)
+import Clay.Filter hiding (url, opacity)
